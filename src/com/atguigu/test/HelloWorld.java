e4b690d1a226860022aeb37d916233ba7d6e1a83
wudaoping <1173515055@qq.com>
2017-07-17 13:04:15 +0800

    Update HelloWorld.java

--- src/com/atguigu/test/HelloWorld.java
+++ src/com/atguigu/test/HelloWorld.java
@@ -3,8 +3,8 @@ package com.atguigu.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world1.0");
-
+		System.out.println("hello world 1.0");
+		System.out.println("hello world 2.0");
 	}
 
 }
